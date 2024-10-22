dd4b4f7926e2b1472f6c6a5ccb562406923d9bbc
ATTENERI MARIA RODRIGUEZ SANCHEZ <attrodsan@alu.edu.gva.es>
2024-10-22 20:53:31 +0200

    BMI exercise. Almost finished.

---
+++ PMDM1/app/src/main/java/com/example/pmdm1/9-bmi.kt
@@ -0,0 +1,24 @@
+package com.example.pmdm1
+
+fun calcBMI(height: Double = 0.0, weight: Double = 0.0): Double {
+    val dividend = height * height
+    val bmi = weight / (height * height)
+    return bmi
+}
+
+fun main() {
+    println("Give me your height in centimeters.")
+    val heightInCentimeters: Double = readln().toDouble()
+    val heightInMeters: Double = heightInCentimeters / 100.0
+    println("Give me your weight in kilograms.")
+    val weight: Double = readln().toDouble()
+    val bmi: Double = calcBMI(heightInMeters, weight)
+    println("BMI: $bmi")
+    when (bmi) {
+        in 0.0..18.4 -> println("UNDERWEIGHT")
+        in 18.5..24.9 -> println("HEALTHY RANGE")
+        in 25.0..29.9 -> println("OVERWEIGHT")
+        in 30.0..39.9 -> println("OBESITY")
+        else -> println("SEVERE OBESITY")
+    }
+}
