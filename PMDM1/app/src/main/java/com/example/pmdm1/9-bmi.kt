d6d92ee82b0c3f4379b2be47e52feab9ea811301
ATTENERI MARIA RODRIGUEZ SANCHEZ <attrodsan@alu.edu.gva.es>
2024-10-30 17:40:14 +0100

    Added project to work with graphic UI.

--- PMDM1/app/src/main/java/com/example/pmdm1/9-bmi.kt
+++ PMDM1/app/src/main/java/com/example/pmdm1/9-bmi.kt
@@ -6,13 +6,7 @@ fun calcBMI(height: Double = 0.0, weight: Double = 0.0): Double {
     return bmi
 }
 
-fun main() {
-    println("Give me your height in centimeters.")
-    val heightInCentimeters: Double = readln().toDouble()
-    val heightInMeters: Double = heightInCentimeters / 100.0
-    println("Give me your weight in kilograms.")
-    val weight: Double = readln().toDouble()
-    val bmi: Double = calcBMI(heightInMeters, weight)
+fun printBMI(bmi: Double) {
     println("BMI: $bmi")
     when (bmi) {
         in 0.0..18.4 -> println("UNDERWEIGHT")
@@ -21,4 +15,14 @@ fun main() {
         in 30.0..39.9 -> println("OBESITY")
         else -> println("SEVERE OBESITY")
     }
+}
+
+fun main() {
+    println("Give me your height in centimeters.")
+    val heightInCentimeters: Double = readln().toDouble()
+    val heightInMeters: Double = heightInCentimeters / 100.0
+    println("Give me your weight in kilograms.")
+    val weight: Double = readln().toDouble()
+    val bmi: Double = calcBMI(heightInMeters, weight)
+    printBMI(bmi)
 }
